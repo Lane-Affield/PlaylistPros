9691fecd8b3c3062e5569b56eeafeb9373c9c5b2
LaneAffield <laneaffield@gmail.com>
2024-03-14 13:17:25 -0500

    Added React FW and cleaning Project

---
+++ src/backend/App.py
@@ -0,0 +1,11 @@
+'''
+Author(s): Lane Affield
+Created: 03/14/24
+Last Update:03/14/24
+Updated By: Lane Affield
+Update Notes: Created File and Framework for file
+
+Description: App for interacting with our website. Makes use of Flask
+
+
+'''
