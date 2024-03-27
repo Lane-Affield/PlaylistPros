11cdd40a811c5838184981325c7be70cd3a9e6e3
LaneAffield <laneaffield@gmail.com>
2024-03-27 18:24:25 -0500

    fixed

--- src/backend/spotify_api_app.py
+++ src/backend/spotify_api_app.py
@@ -1,59 +1,92 @@
+"""
+AUTHORS: Lane Affield , 
+DATE CREATED: 3/27/24
+LAST EDIT:
+LAST EDIT BY:
+EDIT NOTES :
+
+DESCTIPTION:
+    This file is for interacting with the spotify api. 
+    
+
+    NOTE: playing a previous playing a previous song will require more than just a call 
+          - need to keep track of ID of next song and queue it next, then add it back to the api
+
+
+"""
+
+
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os 
 from dotenv import load_dotenv
-from flask import Flask, request, redirect
-from requests_oauthlib import OAuth2Session
-from requests.auth import HTTPBasicAuth
-import requests
-import json
-
-app = Flask(__name__)
-
+from flask import Flask, redirect, request, session, render_template
 
 load_dotenv()
-client_id = os.getenv(
-    "CLIENT_ID"
-)  # client_ID in the .env file, can be found in you rspotify Project Info
+client_id = os.getenv("CLIENT_ID")  # client_ID in the .env file, can be found in you rspotify Project Info
 client_secret = os.getenv("CLIENT_SECRET")  # same as client_id
+redirect_uri = "http://127.0.0.1:8000/callback"  # this is the redirect uri after login
+scopes = "playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-library-read user-library-modify user-top-read user-follow-read streaming user-modify-playback-state user-read-playback-state"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri, scope=scopes)
+)
+app = Flask(__name__)
 
-AUTH_URL = 'https://accounts.spotify.com/authorize'
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
-REDIRECT_URI = 'http://127.0.0.1:3000/' # my case is 'http://localhost:3000/callback'
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-SCOPE = [
-    "playlist-read-private",
-    "playlist-read-collaborative",
-    "playlist-modify-public",
-    "playlist-modify-private",
-    "user-library-read",
-    "user-library-modify",
-    "user-top-read",
-    "user-follow-read",
-    "streaming", 
-    "user-modify-playback-state",
-    "user-read-playback-state"
+#begins session useing spi
+@app.route("/")
+def start():
+    return "session begun"
 
-]
-
-@app.route("/login")
-def login():
-    spotify = OAuth2Session(CLIENT_ID, scope=SCOPE, redirect_uri=REDIRECT_URI)
-    authorization_url, state = spotify.authorization_url(AUTH_URL)
-    return redirect(authorization_url)
-
-@app.route("/callback", methods=['GET'])
+@app.route("/callback")
 def callback():
-    code = request.args.get('code')
-    res = requests.post(TOKEN_URL,
-        auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
-        data={
-            'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': REDIRECT_URI
-        })
-    return json.dumps(res.json())
+    session = start()
+    return session
 
-if __name__ == '__main__':
-    app.run(port=3000,debug=True)
+#pauses a song
+@app.route("/pause")
+def pause_music():
+    sp.pause_playback()
+    return "paused"
+
+
+#plays a song
+@app.route("/play")
+def play_music():
+   sp.start_playback()
+   return "played"
+
+
+#adds a song to queue
+@app.route("/queue/<song_uri>")
+def queue_song(song_uri):
+    sp.add_to_queue(uri=song_uri)
+    return f"queued: {song_uri}"
+
+@app.route("/previous")
+def previous_song():
+    pass
+    
+
+@app.route("/song_info")
+def song_info():
+    current = sp.current_playback()
+    return current
+
+@app.route("/session_info/<ids>")
+def session_info(ids):
+    sp.audio_features(tracks = ids)
+
+@app.route("/closing_time")
+def closing_time():
+    search_results = sp.search(q="Closing Time Semisonic", type="track")
+# Check if search results are empty
+    if len(search_results['tracks']['items']) == 0:
+        print("No songs found for Closing Time by Semisonic")
+    else:
+    # Assuming the first result is the desired song
+        song_uri = [search_results['tracks']['items'][0]['uri']]
+        sp.start_playback(uris=song_uri)
+        return "GET OUT"
+
+app.run(debug=True, port= 8000)
